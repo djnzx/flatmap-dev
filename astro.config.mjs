b66cbd13f4cc3e5e68edb216ba0823910d473213
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2026-04-30 17:40:21 +0300

    fmt only 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,22 @@
 // @ts-check
-import {defineConfig} from "astro/config";
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://example.com",
-    integrations: [
-        mdx({
-            remarkPlugins: [remarkMath],
-            rehypePlugins: [rehypeKatex],
-        }),
-        sitemap()
-    ],
-    adapter: cloudflare({
-        platformProxy: {
-            enabled: true,
-        },
+  site: "https://example.com",
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
     }),
+    sitemap(),
+  ],
+  adapter: cloudflare(),
 });
--- notebook.md
+++ notebook.md
@@ -1,11 +1,34 @@
+### update modules
+
 ```shell
 ncu --u
 npm i
+npm ci
 ```
+
+---
+
+### fix `package-lock.json`
 
 ```shell
 rm package-lock.json
 rm -rf node_modules
 npm install
 npm run build
+```
+
+---
+
+### install prettier
+
+```shell
+npm install -D prettier prettier-plugin-astro
+```
+
+---
+
+### reformat everything
+
+```shell
+npx prettier --write .
 ```
