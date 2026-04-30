ba4590c039fa4f77ac1e94274c2a55946e25a53a
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2026-04-30 13:29:53 +0300

    3

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import {defineConfig} from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
